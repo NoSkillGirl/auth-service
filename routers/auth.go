86674e948b2ab6a9317b9043809cbd14348317a5
NoSkillGirl <singhpooja240393@gmail.com>
2020-01-07 02:26:43 +0530

    changes in docker file

--- routers/auth.go
+++ routers/auth.go
@@ -6,7 +6,7 @@ import (
 	"github.com/NoSkillGirl/auth-service/controllers"
 )
 
-// AuthRoutes - All User related Routes
+// AuthRoutes - All User related Routes.
 func AuthRoutes() {
 	http.HandleFunc("/users/login", controllers.UserLogin)
 	http.HandleFunc("/users/register", controllers.RegisterUser)
